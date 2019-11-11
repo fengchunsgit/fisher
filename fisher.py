c16e56074c7d66cea7a0fb8ce5dd9351106f5d8e
fengchunsgit <1055688803@qq.com>
2019-11-11 18:57:14 +0800

    search isbn

--- fisher.py
+++ fisher.py
@@ -3,13 +3,19 @@ app=Flask(__name__)
 app.config.from_object('config')
 
 
-@app.route("/book/search")
-def search():
+@app.route("/book/search/<q>/<page>")
+def search(q,page):
     """
     q,普通关键字和isbn
     page
     :return:
     """
+    isbn_or_key="key"
+    if len(q)==13 and q.isdigit():
+        isbn_or_key="isbn"
+    short_q=q.replace("-","")
+    if "-" in q and len(short_q)==10 and short_q.isdigit():
+        isbn_or_key="isbn"
     pass
 
 # app.add_url_rule('/hello',view_func=hello)
